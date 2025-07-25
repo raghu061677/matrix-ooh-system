a163412323e51dace8c431170b647c70e7306522
Raghunath Gajula <raghu@matrix-networksolutions.com>
2025-07-25 12:03:52 +0000

    try {   await firebase.firestore().disableNetwork(); // optional   await

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +20,22 @@ const firebaseConfig = {
 const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
+
+// Enable Firestore persistence
+if (typeof window !== 'undefined') {
+    enableIndexedDbPersistence(db)
+      .catch((err) => {
+        if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a time.
+          console.warn('Firestore persistence failed: multiple tabs open.');
+        } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence.
+          console.warn('Firestore persistence not supported in this browser.');
+        }
+      });
+}
+
 
 export { firebaseApp, db, storage };
