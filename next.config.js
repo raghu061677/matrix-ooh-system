b6ed7b31f4703d5dbde9d2104d787c2465688cae
Your Name <you@example.com>
2025-08-05 17:35:11 +0530

    Configure Next.js remote image patterns for placehold.co and storage.googleapis.com

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
